e655d8737615b084ded4bab84471f273a965b624
Kelyan <2431841@bdeb.qc.ca>
2025-11-26 12:15:54 -0500

    debut implementation timeline

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import './style.css';
+import { timeline } from './timeline/timeline.js';
+
+const cercles = document.getElementById("cercles");
+//doit s'executer une fois lors du premier render
+timeline(cercles)
---
+++ src/timeline/timeline.js
@@ -0,0 +1,6 @@
+//chaque iteration de ma boucle va venir rajouter un child a cercles
+export const timeline = ({cercles}) =>{
+    for (let i = 0; i < 10; i++) {
+        
+    }
+}
