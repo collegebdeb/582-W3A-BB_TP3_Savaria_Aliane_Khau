be57196463155d260c27027aac0eecb91a3311f2
Elliot Savari <2472019bdeb.qc.cagit config --global user.name ElliotSavaria$ git config --global user.email 2472019bdeb.qc.ca>
2025-11-28 16:37:58 -0500

    gradiant background timeline canvas

--- src/main.js
+++ src/main.js
@@ -14,12 +14,13 @@ timeline(cercles)
 
 window.addEventListener("resize", (e) => {
     c.width = (window.innerWidth*0.9);
+    c.height = 600
 })
 
 
 
 // variable
-let Pl_Position = 0 ;
+let Pl_Position = -13000 ;
 document.addEventListener("keydown", keypress)
 
 
@@ -29,29 +30,57 @@ let c = document.getElementById("Canvas_Timeline");
 let ctx = c.getContext("2d");
 c.width = (window.innerWidth*0.9)
 
-
+// initialisation gradian
 
 
 
 function Update_Timeline(){
+
+let grad1=ctx.createLinearGradient(0+Pl_Position,0, 16500+Pl_Position,0)
+grad1.addColorStop(0, "blue");
+grad1.addColorStop(0.135, "red");
+grad1.addColorStop(0.255, "blue");
+grad1.addColorStop(0.38, "red");
+grad1.addColorStop(0.5, "blue");
+grad1.addColorStop(0.625, "red");
+grad1.addColorStop(0.745, "blue");
+grad1.addColorStop(0.865, "red");
+grad1.addColorStop(1, "blue");
+
 TextTrigger()
 ctx.fillStyle = "white"
 ctx.fillRect(-10000+Pl_Position,0,120000000000,10000000)
+
+ctx.fillStyle = grad1
+ctx.fillRect(0+Pl_Position,0,16500,800)
 
 
 //
 
 // cube bleu
 ctx.fillStyle = "blue"
-ctx.fillRect(0+Pl_Position,50,500,500)
+ctx.fillRect(100+Pl_Position,50,500,500)
 // cube jaune
 ctx.fillStyle = "yellow"
 ctx.fillRect(2000+Pl_Position,50,500,500)
 // cube vert
 ctx.fillStyle = "green"
-ctx.fillRect(-2000+Pl_Position,50,500,500)
-
-
+ctx.fillRect(4000+Pl_Position,50,500,500)
+// cube mauve
+ctx.fillStyle = "purple"
+ctx.fillRect(6000+Pl_Position,50,500,500)
+// cube rouge
+ctx.fillStyle = "red"
+ctx.fillRect(8000+Pl_Position,50,500,500)
+// cube cyan
+ctx.fillStyle = "aqua"
+ctx.fillRect(10000+Pl_Position,50,500,500)
+// cube orange
+ctx.fillStyle = "orange"
+ctx.fillRect(12000+Pl_Position,50,500,500)
+// cube magenta
+ctx.fillStyle = "magenta"
+ctx.fillRect(14000+Pl_Position,50,500,500)
 
 }
 
@@ -63,11 +92,13 @@ console.log(i.code)
 
 
 if (i.code == "KeyA" ) {
-Pl_Position = Pl_Position-10
+Pl_Position = Pl_Position-100
+if(Pl_Position<-14500){Pl_Position=-14500}
 }
 
 else if (i.code == "KeyD" ) {
-Pl_Position = Pl_Position+10
+Pl_Position = Pl_Position+100
+if(Pl_Position>0){Pl_Position=0}
 }
 
 else if (i.code == "KeyE" ) {
@@ -98,4 +129,9 @@ function TextTrigger(){
             Trigger[i][2] = true;
         }      
     }
+}
+
+function gradian(){
+console.log("test")
+
 }
