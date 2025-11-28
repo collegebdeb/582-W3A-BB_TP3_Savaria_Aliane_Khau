25e4c73c75ddb38567d7e23620a8007cb58e57e7
Kelyan0911 <2431841@bdeb.qc.ca>
2025-11-28 16:54:21 -0500

    Visuel de la timeline

--- src/timeline/timeline.js
+++ src/timeline/timeline.js
@@ -1,6 +1,12 @@
 //chaque iteration de ma boucle va venir rajouter un child a cercles
-export const timeline = ({cercles}) =>{
+// Kelyan Aliane//
+export const timeline = (cercles) =>{
     for (let i = 0; i < 10; i++) {
-        
+        let element = document.createElement('div')
+        element.classList.add('cercle')
+        cercles.appendChild(element);
     }
+
 }
+
+
