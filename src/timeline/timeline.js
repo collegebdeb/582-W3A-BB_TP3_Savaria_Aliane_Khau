258758641d427d4892c25045f1cc2aaf689cfd6e
Kelyan <2431841@bdeb.qc.ca>
2025-12-12 11:41:54 -0500

    Modifcation timeline et arrière plan

--- src/timeline/timeline.js
+++ src/timeline/timeline.js
@@ -1,5 +1,4 @@
-//chaque iteration de ma boucle va venir rajouter un child a cercles
-// Kelyan Aliane//
+// Kelyan Aliane le nombre de cercles dans la timeline
 export const timeline = (cercles) =>{
     for (let i = 0; i < 10; i++) {
         let element = document.createElement('div')
@@ -9,21 +8,12 @@ export const timeline = (cercles) =>{
 
 }
 
-
-// Sélectionne tous les éléments.
+// Agrandissement des cercles dans la timeline
 const elements = document.querySelectorAll(".cercle");
-
-// Détecte le défilement de la page
 document.addEventListener("scroll", ()=>{
-    //Vérifie chacun des éléments
     elements.forEach((elem)=>{
-        
-        //Récupère la position X de l'élément
         const pos = elem.getBoundingClientRect().x;
-        //Récupère le centre de l'écran
         const milieu = window.innerWidth / 2;
-
-        //Ajoute la classe "actif" sur l'élément s'il est dans la zone identifiée.
         if(pos > milieu - 100 && pos < milieu + 100) {
             elem.classList.add("actif");
         } else {
